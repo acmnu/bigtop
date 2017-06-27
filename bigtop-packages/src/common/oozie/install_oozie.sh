7df2dc50695aba86d208976c533fda7221b43e27
Anton Chevychalov <pub@mnu.pp.ru>
2017-06-27 13:16:31 +0300

    ADH-8 Fix 0 size war file

--- bigtop-packages/src/common/oozie/install_oozie.sh
+++ bigtop-packages/src/common/oozie/install_oozie.sh
@@ -209,7 +209,7 @@ mkdir ${WEBAPP_DIR}
 (cd ${WEBAPP_DIR} ; jar xf ${BUILD_DIR}/oozie.war)
 # OOZIE_HOME/lib
 mv -f ${WEBAPP_DIR}/WEB-INF/lib/* ${SERVER_LIB_DIR}/lib/
-touch ${SERVER_LIB_DIR}/webapps/oozie.war
+cp ${BUILD_DIR}/oozie.war ${SERVER_LIB_DIR}/oozie.war
 
 install -m 0755 ${EXTRA_DIR}/tomcat-deployment.sh ${SERVER_LIB_DIR}/tomcat-deployment.sh
 
