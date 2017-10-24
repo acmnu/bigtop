5763633c4775b519b817f9aa944c53cd27c8b23c
Anton Chevychalov <cab@arenadata.io>
2017-10-24 11:47:11 +0300

    ADH-199 Initial hive2 implementation

---
+++ bigtop-packages/src/common/hive2/install_hive.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+usage() {
+  echo "
+usage: $0 <options>
+  Required not-so-options:
+     --build-dir=DIR             path to hive/build/dist
+     --prefix=PREFIX             path to install into
+
+  Optional options:
+     --doc-dir=DIR               path to install docs into [/usr/share/doc/hive]
+     --hive-dir=DIR               path to install hive home [/usr/lib/hive]
+     --installed-hive-dir=DIR     path where hive-dir will end up on target system
+     --bin-dir=DIR               path to install bins [/usr/bin]
+     --examples-dir=DIR          path to install examples [doc-dir/examples]
+     --hcatalog-dir=DIR          path to install hcatalog [/usr/lib/hcatalog]
+     --installed-hcatalog-dir=DIR path where hcatalog-dir will end up on target system
+     ... [ see source for more similar options ]
+  "
+  exit 1
+}
+
+OPTS=$(getopt \
+  -n $0 \
+  -o '' \
+  -l 'prefix:' \
+  -l 'doc-dir:' \
+  -l 'etc-dir:' \
+  -l 'build-dir:' \
+  -l 'version:' -- "$@")
+
+if [ $? != 0 ] ; then
+    usage
+fi
+
+eval set -- "$OPTS"
+while true ; do
+    case "$1" in
+        --prefix)
+        PREFIX=$2 ; shift 2
+        ;;
+        --build-dir)
+        BUILD_DIR=$2 ; shift 2
+        ;;
+        --doc-dir)
+        DOC_DIR=$2 ; shift 2
+        ;;
+        --etc-dir)
+        ETC_DIR=$2 ; shift 2
+        ;;
+        --version)
+        VERSION=$2 ; shift 2
+        ;;
+        --)
+        shift ; break
+        ;;
+        *)
+        echo "Unknown option: $1"
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+for var in PREFIX BUILD_DIR ; do
+  if [ -z "$(eval "echo \$$var")" ]; then
+    echo Missing param: $var
+    usage
+  fi
+done
+
+mkdir -p "$PREFIX/usr/lib"
+cd $PREFIX/usr/lib
+tar xf "${BUILD_DIR}/apache-hive-${VERSION}-src/packaging/target/apache-hive-${VERSION}-bin.tar.gz"
+
+mv apache-hive-2.3.0-bin hive2
+
